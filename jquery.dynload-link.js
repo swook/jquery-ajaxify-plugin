58b2228c8372e59ebbeb68d9380cde4face3197a
Seon-Wook Park <seon.wook@swook.net>
2011-12-02 21:20:17 +0800

    Disable page-caching on exit to avoid conflict with JS init .pageChange

--- jquery.dynload-link.js
+++ jquery.dynload-link.js
@@ -16,6 +16,7 @@
 
 		$.dynload_link.initialState.url = $.dynload_link.current_url;
 		$.dynload_link.initialState.title = document.title;
+		$.dynload_link.page_cache[$.dynload_link.current_url.full] = $('html').html();
 		$.dynload_link.init_done = true;
 		$.dynload_link.all();
 	};
@@ -92,8 +93,6 @@
 			$.dynload_link.gotoAnchor(url.anchor);
 			return;
 		}
-
-		$.dynload_link.page_cache[$.dynload_link.current_url.full] = $('html').html();
 
 		$.get(url.full, function(data) {
 			$.dynload_link.page_cache[url.full] = data;
